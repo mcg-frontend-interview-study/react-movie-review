77b6e261c8e7e7a4fb4b7cff7922c1ed7cdc5a15
ImxYJL <allensain14@gmail.com>
2025-01-24 21:32:07 +0900

    feat: 영화 상세 정보를 불러오는 함수 및 쿼리 훅

--- src/apis/movie.ts
+++ src/apis/movie.ts
@@ -1,5 +1,5 @@
-import {POPULAR_MOVIE_URL, SEARCH_URL} from '../constants/movie';
-import {MovieList} from '../types/movie';
+import {BASE_URL, POPULAR_MOVIE_URL, SEARCH_URL} from '../constants/movie';
+import {MovieDetail, MovieList} from '../types/movie';
 
 export const getMovieList = async ({pageParam = 1}: {pageParam?: number}) => {
   const params = {
@@ -55,4 +55,27 @@ export const getSearchedMovieList = async ({pageParam = 1, title}: GetSearchedMo
 
   const data = await response.json();
   return data as MovieList;
+};
+
+export const getMovieDetail = async (movieId: number) => {
+  const params = {
+    api_key: import.meta.env.VITE_TMDB_TOKEN,
+    language: 'ko-KR',
+  };
+  const endpoint = `${BASE_URL}/movie/${movieId}?${new URLSearchParams(params).toString()}`;
+
+  const response = await fetch(endpoint, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+      accept: 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('영화 상세 정보 불러오는 도중 에러 발생');
+  }
+
+  const data = await response.json();
+  return data as MovieDetail;
 };
---
+++ src/hooks/useGetMovieDetail.ts
@@ -0,0 +1,14 @@
+import {useSuspenseQuery} from '@tanstack/react-query';
+import {getMovieDetail} from '../apis/movie';
+
+const useGetMovieDetail = (movieId: number) => {
+  const result = useSuspenseQuery({
+    queryKey: ['movieDetail', movieId],
+    queryFn: () => getMovieDetail(movieId),
+    staleTime: 24 * 60 * 60 * 1000, // 24시간
+  });
+
+  return result;
+};
+
+export default useGetMovieDetail;
