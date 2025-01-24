42e1c1f83ba2759700dbed83895b29f505cd2af4
ImxYJL <allensain14@gmail.com>
2025-01-24 16:37:20 +0900

    chore: GlobalStyle 적용

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 import {Global} from '@emotion/react';
 import reset from './styles/reset.ts';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import globalStyles from './styles/globalStyles.ts';
 
 async function enableMocking() {
   const {worker} = await import('./mocks/browser.ts');
@@ -37,7 +38,7 @@ enableMocking().then(() => {
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <Global styles={reset} />
+        <Global styles={globalStyles} />
         <App />
       </QueryClientProvider>
     </StrictMode>,
