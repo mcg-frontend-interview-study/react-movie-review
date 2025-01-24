c31f424eaf1f1a0b474fcccc757d9ac490cfc6f8
ImxYJL <allensain14@gmail.com>
2025-01-24 13:36:43 +0900

    chore: 초기 환경설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,44 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import {Global} from '@emotion/react';
+import reset from './styles/reset.ts';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function enableMocking() {
+  const {worker} = await import('./mocks/browser.ts');
+
+  if (import.meta.env.MODE === 'development') {
+    return worker.start();
+  }
+  if (import.meta.env.MODE === 'production') {
+    return worker.start({
+      serviceWorker: {
+        url: 'mockServiceWorker.js',
+      },
+    });
+  }
+}
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+      refetchOnWindowFocus: false,
+    },
+    mutations: {
+      throwOnError: true,
+    },
+  },
+});
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <Global styles={reset} />
+        <App />
+      </QueryClientProvider>
+    </StrictMode>,
+  );
+});
