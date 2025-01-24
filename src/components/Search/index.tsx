057697aca47ccd11506ab0894d5285ed750a9555
ImxYJL <allensain14@gmail.com>
2025-01-24 18:17:31 +0900

    feat: 검색어를 저장하는 atom 및 검색어 설정 기능 추가

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,10 +1,33 @@
+import {useSetAtom} from 'jotai';
 import * as S from './styles';
+import {searchTextAtom} from '../../jotai/atoms';
+import {useState} from 'react';
 
 const Search = () => {
+  const [inputValue, setInputValue] = useState('');
+  const setSearchText = useSetAtom(searchTextAtom);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSearch = () => {
+    setSearchText(inputValue);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      handleSearch();
+    }
+  };
+
   return (
     <S.Search>
-      <input type="text" placeholder="검색" />
-      <button type="submit">검색</button>
+      <input type="text" placeholder="검색" value={inputValue} onChange={handleChange} onKeyDown={handleKeyDown} />
+      <button type="button" onClick={handleSearch}>
+        검색
+      </button>
     </S.Search>
   );
 };
