02a085c740479fb7df906309f0572f40ef1f6cc1
김진호 <rlawlsgh1227@gmail.com>
2025-01-24 16:29:29 +0900

    fix: env 타입 설정 변경

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,15 @@
 /// <reference types="vite-plugin-svgr/client" />
 /// <reference types="vite/client" />
 
-declare namespace NodeJS {
-  interface ProcessEnv {
-    VITE_API_KEY: 'string';
-    VITE_ACCESS_TOKEN: 'string';
-    VITE_API_BASE_URL: 'string';
-    VITE_IMAGE_URL_PREFIX: 'string';
-  }
+interface ImportMetaEnv {
+  VITE_API_KEY: 'string';
+  VITE_ACCESS_TOKEN: 'string';
+  VITE_API_BASE_URL: 'string';
+  VITE_IMAGE_URL_PREFIX: 'string';
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
 
 declare module '*.svg' {
