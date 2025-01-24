6254bd5d6595ed95c3da3c633ee9885f597d913f
김진호 <rlawlsgh1227@gmail.com>
2025-01-24 14:08:36 +0900

    fix: vite env 설정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    VITE_API_KEY: 'string';
+    VITE_ACCESS_TOKEN: 'string';
+    VITE_API_BASE_URL: 'string';
+    VITE_IMAGE_URL_PREFIX: 'string';
+  }
+}
