fe1e1f90b6ec4e93cce04919a85ae12f0fd732b8
김진호 <rlawlsgh1227@gmail.com>
2025-01-24 12:17:35 +0900

    chore: tanstack query 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div>hi</div>;
-}
+import {Main} from './pages/Main';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
-export default App;
+export const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5,
+        gcTime: 1000 * 60 * 5,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Main />;
+    </QueryClientProvider>
+  );
+};
