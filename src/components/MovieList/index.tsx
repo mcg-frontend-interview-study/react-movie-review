ef1f599876ee96576011a7b9cc08c3884c838007
ImxYJL <allensain14@gmail.com>
2025-01-24 16:38:23 +0900

    feat: MovieList 컴포넌트

---
+++ src/components/MovieList/index.tsx
@@ -0,0 +1,44 @@
+import * as S from './styles';
+import useGetList from '../../hooks/useGetList';
+import MovieItem from '../MovieItem';
+import useInfiniteScroll from '../../hooks/useInfiniteScroll';
+
+const MovieList = () => {
+  const {data, hasNextPage, fetchNextPage, isLoading} = useGetList();
+
+  const movies = data.pages.flatMap(page => page.results);
+
+  const lastElementRef = useInfiniteScroll({
+    fetchNextPage,
+    isLoading,
+    isLastPage: !hasNextPage,
+  });
+
+  return (
+    <S.MovieListContainer>
+      <h2>지금 인기 있는 영화</h2>
+      <S.MovieList>
+        {movies.map((movie, index) => {
+          const isLastMovie = index === movies.length - 1;
+
+          return (
+            <div
+              key={movie.id}
+              ref={isLastMovie ? lastElementRef : null} // 마지막 요소에 ref 연결
+            >
+              <MovieItem
+                id={movie.id}
+                title={movie.title}
+                poster_path={movie.poster_path}
+                vote_average={movie.vote_average}
+              />
+            </div>
+          );
+        })}
+      </S.MovieList>
+      {!hasNextPage && <p>No more movies to show.</p>}
+    </S.MovieListContainer>
+  );
+};
+
+export default MovieList;
