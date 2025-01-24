3c38f42414020aca7fde9aab953bacf724da2443
ImxYJL <allensain14@gmail.com>
2025-01-24 18:18:23 +0900

    feat: 검색어 유무에 따라 영화 목록 데이터를 불러오는 쿼리 훅

--- src/hooks/useGetList.ts
+++ src/hooks/useGetList.ts
@@ -1,20 +1,23 @@
 import {useSuspenseInfiniteQuery} from '@tanstack/react-query';
-import {getMovieList} from '../apis/movie';
+import {getMovieList, getSearchedMovieList} from '../apis/movie';
+import {useAtomValue} from 'jotai';
+import {searchTextAtom} from '../jotai/atoms';
 
 const useGetList = () => {
-  const result = useSuspenseInfiniteQuery({
-    queryKey: ['movieList'],
-    queryFn: ({pageParam}) => getMovieList({pageParam}),
+  const searchText = useAtomValue(searchTextAtom);
 
+  const result = useSuspenseInfiniteQuery({
+    queryKey: searchText ? ['searchedMovieList', searchText] : ['movieList'],
+    queryFn: ({pageParam = 1}) =>
+      searchText ? getSearchedMovieList({pageParam, title: searchText}) : getMovieList({pageParam}),
     initialPageParam: 1,
     getNextPageParam: lastPage => {
       return lastPage.page < lastPage.total_pages ? lastPage.page + 1 : undefined;
     },
-
     staleTime: 30 * 60 * 1000, // 30분
   });
 
-  return {...result};
+  return result;
 };
 
 export default useGetList;
