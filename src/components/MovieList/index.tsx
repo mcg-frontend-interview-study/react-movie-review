5ac04ad6e84b598d83c1ca4d01c9c627617978bf
ImxYJL <allensain14@gmail.com>
2025-01-25 22:11:53 +0900

    fix: isLoading과 isFetchingNextPage 분리

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -37,17 +37,38 @@ const MovieList = () => {
     if (selectedItem) setSelectedMovieId(id);
   };
 
-  const isFetching = isLoading || isFetchingNextPage;
-
   return (
     <S.MovieListContainer>
       <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
         <h2>{searchText ? `"${searchText}" 검색 결과` : '지금 인기 있는 영화'}</h2>
 
         <S.MovieList onClick={handleItemClick}>
-          {isFetching && <SkeletonMovieList />}
+          {isLoading && <SkeletonMovieList />}
 
-          {!isFetching &&
+          {!isLoading &&
+            movies.map((movie, index) => {
+              const isLastMovie = index === movies.length - 1;
+
+              return (
+                <>
+                  <div
+                    key={movie.id}
+                    ref={isLastMovie ? lastElementRef : null} // 마지막 요소에 ref 연결
+                  >
+                    <MovieItem
+                      key={movie.id}
+                      id={movie.id}
+                      title={movie.title}
+                      poster_path={movie.poster_path}
+                      vote_average={movie.vote_average}
+                    />
+                  </div>
+                </>
+              );
+            })}
+
+          {isFetchingNextPage && <SkeletonMovieList />}
+          {!isFetchingNextPage &&
             movies.map((movie, index) => {
               const isLastMovie = index === movies.length - 1;
 
