85da46ab4bc3d332c0361a86180d4870f7665f5e
김진호 <rlawlsgh1227@gmail.com>
2025-01-30 22:17:21 +0900

    feat: 이미지 컴포넌트 생성 (load 스켈레톤 보여주기 위해)

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+import {containerStyle, skeletonStyle} from './style';
+
+type ImageProps = React.ComponentProps<'img'>;
+
+export const Image = ({width, height, src, alt, ...imgProps}: ImageProps) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  console.log(isLoaded);
+
+  return (
+    <div css={containerStyle}>
+      <img src={src} alt={alt} {...imgProps} onLoad={() => setIsLoaded(true)} />
+      {!isLoaded && <div css={skeletonStyle(width, height)}></div>}
+    </div>
+  );
+};
