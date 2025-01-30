f8321a42dc3d60856bb56211e64e65dfa8d309ec
bmj0053 <bmj05378@gmail.com>
2025-01-30 15:35:46 +0900

    refactor: Header props extends 제거 및 Layout html 태그를 header로 변경

--- src/components/_common/Header/Header.styled.ts
+++ src/components/_common/Header/Header.styled.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import media from '../../../styles/mediaQueries';
 
-export const Layout = styled.div`
+export const Layout = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
--- src/components/_common/Header/Header.tsx
+++ src/components/_common/Header/Header.tsx
@@ -2,18 +2,18 @@ import { DISPLAY_SIZE } from '../../../constants/displaySize';
 import LargeHeader from './LargeHeader';
 import MobileHeader from './MobileHeader';
 
-interface HeaderProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface HeaderProps {
   searchKeyword: string;
   onSearch: (keyword: string) => void;
 }
 
-function Header({ searchKeyword, onSearch, ...rest }: HeaderProps) {
+function Header({ searchKeyword, onSearch }: HeaderProps) {
   const isMobile = document.documentElement.clientWidth <= DISPLAY_SIZE.mobile;
 
   return isMobile ? (
-    <MobileHeader searchKeyword={searchKeyword} onSearch={onSearch} {...rest} />
+    <MobileHeader searchKeyword={searchKeyword} onSearch={onSearch} />
   ) : (
-    <LargeHeader searchKeyword={searchKeyword} onSearch={onSearch} {...rest} />
+    <LargeHeader searchKeyword={searchKeyword} onSearch={onSearch} />
   );
 }
 
