c663143a66091ee5a24e0877d3a53c84eb0468fc
ImxYJL <allensain14@gmail.com>
2025-02-01 23:01:37 +0900

    chore: 불필요한 React.FC 제거

--- src/components/common/ModalBackground/index.tsx
+++ src/components/common/ModalBackground/index.tsx
@@ -1,16 +1,17 @@
-import React, {PropsWithChildren, useRef} from 'react';
+import React, {useRef} from 'react';
 
 import * as S from './styles.ts';
 import useModalClose from '../../../hooks/useModalClose.ts';
 
 interface ModalBackgroundProps {
   closeModal: (() => void) | null;
+  children?: React.ReactNode;
 }
-const ModalBackground: React.FC<PropsWithChildren<ModalBackgroundProps>> = ({children, closeModal}) => {
+
+const ModalBackground = ({children, closeModal}: ModalBackgroundProps) => {
   const modalBackgroundRef = useRef<HTMLDivElement>(null);
   useModalClose({closeModal, modalBackgroundRef});
 
   return <S.ModalBackground ref={modalBackgroundRef}>{children}</S.ModalBackground>;
 };
-
 export default ModalBackground;
