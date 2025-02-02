700c87ec5927add53acfaff63f4ddbe1087c9728
ImxYJL <allensain14@gmail.com>
2025-02-02 20:29:39 +0900

    feat: 반응형 헤더

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,27 @@
 import Search from '../Search';
 import movieLogo from '../../assets/logo.png';
 import * as S from './styles';
+import {useEffect, useState} from 'react';
+import MobileSearch from '../MobileSearch';
+
+const MOBILE_WIDTH = 440;
 
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < MOBILE_WIDTH);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <S.Header>
       <h1>
@@ -10,7 +29,7 @@ const Header = () => {
           <img src={movieLogo} alt="MovieList 로고" />
         </S.LogoWrapper>
       </h1>
-      <Search />
+      {isMobile ? <MobileSearch /> : <Search />}
     </S.Header>
   );
 };
--- src/components/MobileSearch/styles.ts
+++ src/components/MobileSearch/styles.ts
@@ -8,6 +8,7 @@ export const Search = styled.section`
   background: #fff;
   padding: 8px;
   border-radius: 4px;
+  position: relative;
 
   button {
     width: 14px;
@@ -15,13 +16,29 @@ export const Search = styled.section`
     text-indent: -1000rem;
     background: url(${searchImg}) no-repeat center center;
     background-size: contain;
+    z-index: 2;
+    position: relative;
+    height: 100%;
   }
 `;
 
-export const SearchInput = styled.input`
+export const SearchInput = styled.input<{$isInputOpen: boolean}>`
   font-size: 13px;
 
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+
   border: 0;
+  padding: 8px;
+  background: white;
+  border-radius: 4px;
+
+  transition: width 0.3s ease-in-out, opacity 0.3s ease-in-out;
+
+  width: ${({$isInputOpen}) => ($isInputOpen ? '88vw' : '0px')};
+  opacity: ${({$isInputOpen}) => ($isInputOpen ? 1 : 0)};
 
   &:focus {
     outline: none;
