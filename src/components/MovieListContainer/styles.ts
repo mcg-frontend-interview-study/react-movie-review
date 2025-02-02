18f684117f8502c77639f4dac227fa0e223b9bff
ImxYJL <allensain14@gmail.com>
2025-02-02 17:11:50 +0900

    chore: 누락된 styles 파일 업로드

--- src/components/MovieListContainer/styles.ts
+++ src/components/MovieListContainer/styles.ts
@@ -28,35 +28,3 @@ export const ContentContainer = styled.div`
   flex-direction: column;
   justify-content: center;
 `;
-
-export const MovieList = styled.ul`
-  display: grid;
-  margin: 48px 0;
-  grid-template-columns: repeat(4, 180px);
-  grid-column-gap: 160px;
-  grid-row-gap: 48px;
-
-  @media (min-width: 768px) and (max-width: 1399px) {
-    grid-template-columns: repeat(3, 140px);
-    grid-column-gap: 100px;
-  }
-
-  @media (min-width: 435px) and (max-width: 767px) {
-    grid-template-columns: repeat(2, 150px);
-    grid-column-gap: 90px;
-    grid-row-gap: 45px;
-  }
-
-  @media (max-width: 434px) {
-    grid-template-columns: 1fr;
-    grid-column-gap: 0;
-    grid-row-gap: 24px;
-  }
-`;
-
-export const Message = styled.p`
-  font-size: 1.2rem;
-  height: 85vh;
-  width: 100%;
-  margin-top: 2.5rem;
-`;
